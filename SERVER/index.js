9343bdccf31fb82654cdfd3e23b833278c1652ac
Vikas-922 <vikasvish.9922@gmail.com>
2024-11-14 17:37:27 +0530

    Update index.js

--- SERVER/index.js
+++ SERVER/index.js
@@ -8,11 +8,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors(
-  //   {
-  //     origin: ["http://127.0.0.1:5500"],
-  //     methods: ["POST", "GET"],
-  //     credentials: true
-  // }
+    {
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET"],
+    credentials: true
+}
+
   )); // Enable CORS for all routes abc
 
 let correctAnswers = [];
